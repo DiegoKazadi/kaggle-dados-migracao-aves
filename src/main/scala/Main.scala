e82a0cbeb27bb09bb6efcae881092d4aca45a9fd
Diego Kazadi <diegokazadi@gmail.com>
2025-05-12 10:01:38 -0300

    Algumas alterações na pasta build.sbt e Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,16 +3,16 @@ import org.apache.spark.sql.SparkSession
 object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .appName("Analise Migracao Aves")
+      .appName("Analise de Dados - Kaggle")
       .master("local[*]")
       .getOrCreate()
 
-    val df = spark.read.option("header", "true").option("inferSchema", "true")
-      .csv("dados/migracao_aves.csv")
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("data/StudentPerformance.csv")
 
-    df.printSchema()
-    df.show(5)
-
+    df.show(10)
     spark.stop()
   }
 }
